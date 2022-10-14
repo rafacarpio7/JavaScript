f95e56caffc87337e8bc5b3785ed426cad10f219
rafacarpio7 <rafaeltredondo6@gmail.com>
2022-10-14 10:49:01 +0200

    Changes to be committed: 	new file:   cacharreo.js

---
+++ cacharreo.js
@@ -0,0 +1,13 @@
+var persona = {
+    nombre : 'Rafa',
+    apellidos:'Torralba Redondo' ,
+    telefono: 619866709,
+    dni: '48122234R',
+    peso : 67,
+    altura : 1.72 
+}
+
+console.log(persona);
+
+
+
